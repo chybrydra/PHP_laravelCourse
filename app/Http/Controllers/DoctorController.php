472509a130bf7ae90f7b75571e7e5b6e2f3d8d47
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-02-11 01:43:53 +0100

    create blade template

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -12,6 +12,8 @@ class DoctorController extends Controller{
     		array("id"=>2, "lastname"=>"Austin", "firstname"=>"Adam", "email"=>"adam@austin.com", "phone"=>"666333666", "address"=>"Address 2", "status"=>"Dostępny"),
     		array("id"=>3, "lastname"=>"Carrson", "firstname"=>"Bob", "email"=>"bob@carrson.com", "phone"=>"987654321", "address"=>"Address 3", "status"=>"Niedostępny")
     	);
-    	return view('doctors.list', ["doctorsList"=>$doctorsList]);
+    	return view('doctors.list', ["doctorsList"	=>	$doctorsList,
+									"footerYear"	=>	date("Y"),
+									"title"			=>	"Moduł lekarzy"]);
     }
 }
